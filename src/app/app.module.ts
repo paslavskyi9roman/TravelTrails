4df7bb615e8f50d78b774f781a5338d41a5b019b
paslavsky9roman <46484914+paslavskyi9roman@users.noreply.github.com>
2023-06-11 23:53:42 +0300

    creating base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './features/auth/auth.component';
+import { MapComponent } from './components/map/map.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MapComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
